c8ece979a627606103d0483cbbc6590b1b0ef44f
sunghwankwon <skwon25@myseneca.ca>
2020-01-15 11:17:20 -0500

    web322 assignment

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /*
+    This is solely my work.
     Name: sunghwan kwon
     ID: skwon25
-    Heroku Link: 
+    Heroku Link: https://git.heroku.com/guarded-bayou-54854.git
     
 */
 
-var HTTP_PORT = process.env.PROT || 3000;
+var HTTP_PORT = process.env.PROT || 8080;
 var express = require("express");
 var app = express();
-
 
 app.get("/", (req,res)=>{
     res.send("Sunghwan Kwon, skwon25");
